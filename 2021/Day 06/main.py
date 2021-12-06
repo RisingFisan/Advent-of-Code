4b509d20fcaf349af7e0d7690016d75ed4813ce6
Sofia Santos <sofiarsantos@gmail.com>
2021-12-06 10:07:34 +0000

    finished 2021 day 6

--- 2021/Day 06/main.py
+++ 2021/Day 06/main.py
@@ -3,57 +3,36 @@ from sys import argv
 with open(argv[1] if len(argv) > 1 else "input") as f:
     numbers = [int(x) for x in f.read().split(',')]
 
-# for day in range(80):
-#     for i in range(len(numbers)):
-#         if numbers[i] == 0:
-#             numbers.append(8)
-#             numbers[i] = 6
-#         else:
-#             numbers[i] -= 1
+states = {
+    0: 0,
+    1: 0,
+    2: 0,
+    3: 0,
+    4: 0,
+    5: 0,
+    6: 0,
+    7: 0,
+    8: 0
+}
 
-numbers1 = [n-80 for n in numbers]
-part1 = 0
+for n in numbers:
+    states[n] += 1
 
-while True:
-    changes = False
-    try:
-        n = numbers1.pop(0)
-        if n < 0:
-            new_n = n % 7
-            offset = 6 - new_n
-            for j in range(abs(n // 7)):
-                numbers1.append(8 - offset - 7 * j)
-        part1 += 1
-    except IndexError:
-        break
+for i in range(80):
+    tmp = states[0]
+    for i in range(8):
+        states[i] = states[i+1]
+    states[6] += + tmp
+    states[8] = tmp
 
-print(f"Part 1: {part1}")
+print(f"Part 1: {sum(states.values())}")
 
-# for day in range(256 - 80):
-#     print(80+day)
-#     for i in range(len(numbers)):
-#         if numbers[i] == 0:
-#             numbers.append(8)
-#             numbers[i] = 6
-#         else:
-#             numbers[i] -= 1
+for i in range(256 - 80):
+    tmp = states[0]
+    for i in range(8):
+        states[i] = states[i+1]
+    states[6] += + tmp
+    states[8] = tmp
 
-# print(f"Part 2: {len(numbers)}")
-
-numbers2 = [n-256 for n in numbers]
-part2 = 0
-
-while True:
-    changes = False
-    try:
-        n = numbers2.pop(0)
-        if n < 0:
-            new_n = n % 7
-            offset = 6 - new_n
-            for j in range(abs(n // 7)):
-                numbers2.append(8 - offset - 7 * j)
-        part2 += 1
-    except IndexError:
-        break
-
-print(f"Part 2: {len(numbers)}")
+print(f"Part 2: {sum(states.values())}")
+    
